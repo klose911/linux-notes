a00b986833260c51ca32e2813a7cf6487b20bbe8
klose911 <klose911@gmail.com>
2018-11-21 21:59:22 +0800

    calcualte Unix time from cmos time

--- include/time.h
+++ include/time.h
@@ -1,9 +1,16 @@
+/**
+ * 用于涉及处理时间和日期的函数，这是标准C库中的头文件之一。
+ * 当前版本中主要为 init/main.c 和 kernel/mktime.c 提供 tm 结构类型，用于内核从系统 CMOS 芯片读取实时时钟信息（日历时间），从而可以设定系统开机时间
+ * 系统开机时间是指从 1970年1月1日午夜0时起所经过的时间（秒），它将保存在全局变量 startup_time 中供内核所有代码读取
+ *
+ * 另外，这里的一些函数声明均是标准 C 库提供的函数，内核中不包括这些函数
+ */
 #ifndef _TIME_H
 #define _TIME_H
 
 #ifndef _TIME_T
 #define _TIME_T
-typedef long time_t;
+typedef long time_t; // 从 GMT 1970年1月1日午夜0时起所经过的时间（秒）
 #endif
 
 #ifndef _SIZE_T
@@ -11,25 +18,27 @@ typedef long time_t;
 typedef unsigned int size_t;
 #endif
 
-#define CLOCKS_PER_SEC 100
+#define CLOCKS_PER_SEC 100 // 系统时间滴答频率 100HZ 
 
-typedef long clock_t;
+typedef long clock_t; // 从进程开始执行计起的系统经过的时钟滴答数
 
 struct tm {
-	int tm_sec;
-	int tm_min;
-	int tm_hour;
-	int tm_mday;
-	int tm_mon;
-	int tm_year;
-	int tm_wday;
-	int tm_yday;
-	int tm_isdst;
+        int tm_sec; // 秒数 [0, 59]
+        int tm_min; // 分钟数 [0,59]
+        int tm_hour; // 小时数字 [0,23]
+        int tm_mday; // 一个月的天数 [0,31] 
+        int tm_mon; // 一年中的月份 [0, 11]
+        int tm_year; //  从 1900 年开始的年数
+        int tm_wday; //  一星期中的某天 [0, 6] （星期天 = 0）
+        int tm_yday; //  一年中的某天 [0, 365] 
+        int tm_isdst; // 夏令时标志，正数：使用，0：没有使用，负数：无效
 };
 
+// 以下是有关时间操作的函数原型
 clock_t clock(void);
 time_t time(time_t * tp);
 double difftime(time_t time2, time_t time1);
+// 将 tm 结构表示的时间 转换成 从1970年1月1日午夜0时起所经过的时间（秒）
 time_t mktime(struct tm * tp);
 
 char * asctime(const struct tm * tp);
--- kernel/mktime.c
+++ kernel/mktime.c
@@ -17,42 +17,58 @@
  * PS. I hate whoever though up the year 1970 - couldn't they have gotten
  * a leap-year instead? I also hate Gregorius, pope or no. I'm grumpy.
  */
-#define MINUTE 60
-#define HOUR (60*MINUTE)
-#define DAY (24*HOUR)
-#define YEAR (365*DAY)
+
+/**
+ * 这不是标准C库函数，仅供内核使用。因此并不关心小于1970年以前的年份，但假定一切均正常
+ * 类似地，时区 TZ 这里也可以忽略掉，尽可能简单地处理问题
+ */
+
+#define MINUTE 60 // 一分钟的秒数
+#define HOUR (60*MINUTE) // 一小时的秒数
+#define DAY (24*HOUR) // 一天的秒数
+#define YEAR (365*DAY) // 一年的秒数（非闰年！！！）
 
 /* interestingly, we assume leap-years */
+// 下面以年为界限，定义了每个月开始的秒数
+// 注意：这里假定是闰年！！！
 static int month[12] = {
-	0,
-	DAY*(31),
-	DAY*(31+29),
-	DAY*(31+29+31),
-	DAY*(31+29+31+30),
-	DAY*(31+29+31+30+31),
-	DAY*(31+29+31+30+31+30),
-	DAY*(31+29+31+30+31+30+31),
-	DAY*(31+29+31+30+31+30+31+31),
-	DAY*(31+29+31+30+31+30+31+31+30),
-	DAY*(31+29+31+30+31+30+31+31+30+31),
-	DAY*(31+29+31+30+31+30+31+31+30+31+30)
+        0,
+        DAY*(31),
+        DAY*(31+29), // 闰年
+        DAY*(31+29+31),
+        DAY*(31+29+31+30),
+        DAY*(31+29+31+30+31),
+        DAY*(31+29+31+30+31+30),
+        DAY*(31+29+31+30+31+30+31),
+        DAY*(31+29+31+30+31+30+31+31),
+        DAY*(31+29+31+30+31+30+31+31+30),
+        DAY*(31+29+31+30+31+30+31+31+30+31),
+        DAY*(31+29+31+30+31+30+31+31+30+31+30)
 };
 
+/**
+ * 计算从1970年1月1日午夜0时起所经过的时间（秒），作为开机时间
+ *
+ * tm: 结构中的各字段已经在 init/main.c 中被赋值，信息取自 CMOS 
+ */
 long kernel_mktime(struct tm * tm)
 {
-	long res;
-	int year;
+        long res;
+        int year;
 
-	year = tm->tm_year - 70;
+        //计算从 1970年 开始经过的年份数
+        year = tm->tm_year - 70; // 由于只用”2位数“表示年份，所以会有千年虫问题
 /* magic offsets (y+1) needed to get leapyears right.*/
-	res = YEAR*year + DAY*((year+1)/4);
-	res += month[tm->tm_mon];
+        // 由于 unix 年份从1970年开始，1972年是第一个闰年，从1973年开始需要计入闰年多出来的一天，以后每隔4年就是一个闰年
+        // 因此闰年数的计算方法是 1 + (y - 3) / 4 = (y + 1) / 4 
+        res = YEAR*year + DAY*((year+1)/4); // 计算从 1970年1月1号开始到去年12月31号的秒数
+        res += month[tm->tm_mon]; // 今年到上个月为止过去的秒数，注意如果月份大于2，这里已经计入了闰年多出一天的秒数，所以下面需要判断今年是否是闰年，来决定是否做调整
 /* and (y+2) here. If it wasn't a leap-year, we have to adjust */
-	if (tm->tm_mon>1 && ((year+2)%4))
-		res -= DAY;
-	res += DAY*(tm->tm_mday-1);
-	res += HOUR*tm->tm_hour;
-	res += MINUTE*tm->tm_min;
-	res += tm->tm_sec;
-	return res;
+        if (tm->tm_mon>1 && ((year+2)%4)) // tm->tm_mon > 1 => 月份大于2 ， (year + 2) 不能被 4 整除 => 今年不是闰年 
+                res -= DAY; // 这时候需要减去一天 
+        res += DAY*(tm->tm_mday-1); // 加上本月过去的天数的秒数时间
+        res += HOUR*tm->tm_hour; // 加上今天过去的小时的秒数时间
+        res += MINUTE*tm->tm_min; // 加上当前小时过去的分钟的秒数时间
+        res += tm->tm_sec; // 加上当前分钟过去的秒数时间
+        return res;
 }
