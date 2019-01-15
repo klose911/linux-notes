d21995d740e34932dda6c3c2169b7f363ae0cc98
klose911 <klose911@gmail.com>
2019-01-15 22:29:06 +0800

    truncate file/directory functions

--- fs/truncate.c
+++ fs/truncate.c
@@ -4,62 +4,103 @@
  *  (C) 1991  Linus Torvalds
  */
 
-#include <linux/sched.h>
+#include <linux/sched.h> // 内核调度头文件
 
-#include <sys/stat.h>
+#include <sys/stat.h> // 文件状态头文件，含有文件或文件系统状态结构 stat{} 和常量
 
+/*
+ * 释放所有一级间接块
+ *
+ * dev: 设备号
+ * block: 一次间接块号(i_zone[7])
+ *
+ * 无返回值
+ * 
+ */
 static void free_ind(int dev,int block)
 {
-	struct buffer_head * bh;
-	unsigned short * p;
-	int i;
+        struct buffer_head * bh;
+        unsigned short * p;
+        int i;
 
-	if (!block)
-		return;
-	if ((bh=bread(dev,block))) {
-		p = (unsigned short *) bh->b_data;
-		for (i=0;i<512;i++,p++)
-			if (*p)
-				free_block(dev,*p);
-		brelse(bh);
-	}
-	free_block(dev,block);
+        if (!block) // 校验逻辑块号是否为0
+                return; // 如果为0，则直接返回
+
+        // 把设备上对应的一次间接块读入到高速缓冲区
+        if ((bh=bread(dev,block))) {
+                p = (unsigned short *) bh->b_data; // 指向该缓冲头的数据区
+                // 按照２字节遍历这个缓冲区（每个i节点是２个字节）
+                for (i=0;i<512;i++,p++) {
+                        if (*p) { 
+                                free_block(dev,*p); // 释放指定的设备上的逻辑块（这些都是数据块）
+                        }
+                }
+                brelse(bh); // 释放间接块占用的高速缓冲区中的缓冲块
+        }
+        free_block(dev,block); // 释放设备上的一次间接块
 }
 
+
+/*
+ * 释放所有二次间接块
+ *
+ * dev: 设备号
+ * block: 二次间接块号(i_zone[8])
+ *
+ * 无返回值
+ * 
+ */
 static void free_dind(int dev,int block)
 {
-	struct buffer_head * bh;
-	unsigned short * p;
-	int i;
+        struct buffer_head * bh;
+        unsigned short * p;
+        int i;
 
-	if (!block)
-		return;
-	if ((bh=bread(dev,block))) {
-		p = (unsigned short *) bh->b_data;
-		for (i=0;i<512;i++,p++)
-			if (*p)
-				free_ind(dev,*p);
-		brelse(bh);
-	}
-	free_block(dev,block);
+        if (!block)
+                return;
+        if ((bh=bread(dev,block))) {
+                p = (unsigned short *) bh->b_data;
+                for (i=0;i<512;i++,p++) {
+                        if (*p) {
+                                // 代码和 free_ind 只有这里不同，这里是再次调用 free_ind来释放对应的二次间接块的数据块
+                                free_ind(dev,*p);
+                        }
+                }
+                brelse(bh);
+        }
+        free_block(dev,block);
 }
 
+/**
+ * 截断文件数据函数
+ *
+ * inode: 文件对应的内存中的i节点指针
+ *
+ * 无返回值
+ *
+ * 将节点对应的文件长度截为０，并释放占用的设备空间
+ *
+ */
 void truncate(struct m_inode * inode)
 {
-	int i;
+        int i;
 
-	if (!(S_ISREG(inode->i_mode) || S_ISDIR(inode->i_mode)))
-		return;
-	for (i=0;i<7;i++)
-		if (inode->i_zone[i]) {
-			free_block(inode->i_dev,inode->i_zone[i]);
-			inode->i_zone[i]=0;
-		}
-	free_ind(inode->i_dev,inode->i_zone[7]);
-	free_dind(inode->i_dev,inode->i_zone[8]);
-	inode->i_zone[7] = inode->i_zone[8] = 0;
-	inode->i_size = 0;
-	inode->i_dirt = 1;
-	inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+        // 只有常规文件或目录文件，才可以被截断为0
+        if (!(S_ISREG(inode->i_mode) || S_ISDIR(inode->i_mode)))
+                return; // 非常规文件和目录文件，直接返回
+
+        // 遍历这个 i节点中的直接块号数组：i_zone[0] ~ i_zone[6]
+        for (i=0;i<7;i++) {
+                if (inode->i_zone[i]) {
+                        free_block(inode->i_dev,inode->i_zone[i]); // 释放对应的数据块
+                        inode->i_zone[i]=0; // 数据块号字段设置为 0 
+                }
+        }
+        free_ind(inode->i_dev,inode->i_zone[7]); // 释放所有的一次间接块：i_zone[7]
+        free_dind(inode->i_dev,inode->i_zone[8]); // 释放所有的二次间接块：i_zone[8]
+        inode->i_zone[7] = inode->i_zone[8] = 0; // 一次间接块号，二次间接块号都设置为 0
+
+        inode->i_size = 0; // i节点的字节大小设置为 0  
+        inode->i_dirt = 1; // i节点的修改标志设置为 真
+        inode->i_mtime = inode->i_ctime = CURRENT_TIME; // i节点的修改时间 = i节点的创建时间　设置为当前 unix 时间（秒数）
 }
-
--- include/sys/stat.h
+++ include/sys/stat.h
@@ -4,50 +4,59 @@
 #include <sys/types.h>
 
 struct stat {
-	dev_t	st_dev;
-	ino_t	st_ino;
-	umode_t	st_mode;
-	nlink_t	st_nlink;
-	uid_t	st_uid;
-	gid_t	st_gid;
-	dev_t	st_rdev;
-	off_t	st_size;
-	time_t	st_atime;
-	time_t	st_mtime;
-	time_t	st_ctime;
+        dev_t	st_dev; // 普通文件的设备号
+        ino_t	st_ino; // 文件的“i节点”号
+        umode_t	st_mode; // 文件类型和属性
+        nlink_t	st_nlink; //文件的硬连接数
+        uid_t	st_uid; // 文件的用户ID 
+        gid_t	st_gid; // 文件的组ID
+        dev_t	st_rdev; // 设备号（特殊的字符文件或块文件）
+        off_t	st_size; // 文件大小（字节数）（如果文件是常规文件的话）
+        time_t	st_atime; // 上次（最后）访问时间
+        time_t	st_mtime; // 最后修改时间
+        time_t	st_ctime; // 最后节点修改时间
 };
 
-#define S_IFMT  00170000
-#define S_IFREG  0100000
-#define S_IFBLK  0060000
-#define S_IFDIR  0040000
-#define S_IFCHR  0020000
-#define S_IFIFO  0010000
-#define S_ISUID  0004000
-#define S_ISGID  0002000
-#define S_ISVTX  0001000
+//下面是为st_mode字段所用的值定义的符号名称，这些值均用八进制表示
+// st_mode 由三部分组成：文件类型 | 文件属性 | 文件访问权限
 
-#define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)
-#define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
-#define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
-#define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
-#define S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)
+// 文件类型
+#define S_IFMT  00170000 // 文件类型位屏蔽码
+#define S_IFREG  0100000 // 常规文件
+#define S_IFBLK  0060000 // 块文件，如 /dev/fd0 等
+#define S_IFDIR  0040000 // 目录文件
+#define S_IFCHR  0020000 // 字符设备文件
+#define S_IFIFO  0010000 // 管道特殊文件
 
-#define S_IRWXU 00700
-#define S_IRUSR 00400
-#define S_IWUSR 00200
-#define S_IXUSR 00100
+//文件属性位
+// S_ISUID 如果被设置，那么当执行该文件时，“进程的有效用户ID”被设置为“该文件的宿主用户ID”　
+// S_ISGID 处理和 S_ISUID 类似
+#define S_ISUID  0004000 // 执行时设置用户ID(set-user-ID) 
+#define S_ISGID  0002000 // 执行时设置组ID(set-group-ID) 
+#define S_ISVTX  0001000 // 对于目录，受限删除标志
 
-#define S_IRWXG 00070
-#define S_IRGRP 00040
-#define S_IWGRP 00020
-#define S_IXGRP 00010
+#define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG) // 测试是否是“常规”文件
+#define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR) // 测试是否是“目录”文件
+#define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR) // 测试是否是“字符设备”文件
+#define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK) // 测试是否是“块设备”文件
+#define S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO) // 测试是否是“管道”特殊文件
 
-#define S_IRWXO 00007
-#define S_IROTH 00004
-#define S_IWOTH 00002
-#define S_IXOTH 00001
+// 文件访问权限
+#define S_IRWXU 00700 // 宿主有　读，写，执行／搜索　权限
+#define S_IRUSR 00400 // 宿主有　读 权限
+#define S_IWUSR 00200 // 宿主有　写 权限
+#define S_IXUSR 00100 // 宿主有　执行／搜索　权限
 
+#define S_IRWXG 00070 // 组成员有　读，写，执行／搜索　权限
+#define S_IRGRP 00040 // 组成员有　读 权限
+#define S_IWGRP 00020 // 组成员有　写 权
+#define S_IXGRP 00010 // 组成员有　执行／搜索　权限
+
+#define S_IRWXO 00007 // 其他人有　读，写，执行／搜索　权限
+#define S_IROTH 00004 // 其他人有　读 权限
+#define S_IWOTH 00002 // 其他人有　写 权
+#define S_IXOTH 00001 // 其他人有　执行／搜索　权限
+        
 extern int chmod(const char *_path, mode_t mode);
 extern int fstat(int fildes, struct stat *stat_buf);
 extern int mkdir(const char *_path, mode_t mode);
