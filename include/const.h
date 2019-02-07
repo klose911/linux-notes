b75a3f00665d55a6b1c66f90e34c47f5f6de0de1
klose911 <klose911@gmail.com>
2019-02-07 22:46:16 +0800

    fcntl header file && accmode macro

--- include/const.h
+++ include/const.h
@@ -1,15 +1,19 @@
 #ifndef _CONST_H
 #define _CONST_H
 
-#define BUFFER_END 0x200000
+/**
+ * 该文件定义了i节点中的文件属性类型和i_mode字段所用到的一些标志位常量符号
+ */
+#define BUFFER_END 0x200000 // 缓冲使用的内存末端
 
-#define I_TYPE          0170000
-#define I_DIRECTORY	0040000
-#define I_REGULAR       0100000
-#define I_BLOCK_SPECIAL 0060000
-#define I_CHAR_SPECIAL  0020000
-#define I_NAMED_PIPE	0010000
-#define I_SET_UID_BIT   0004000
-#define I_SET_GID_BIT   0002000
+// i节点数据结构中的i_mode字段的各标志位
+#define I_TYPE          0170000 // 指明i节点类型
+#define I_DIRECTORY	    0040000 // 目录文件
+#define I_REGULAR       0100000 // 常规文件
+#define I_BLOCK_SPECIAL 0060000 // 块设备特殊文件
+#define I_CHAR_SPECIAL  0020000 // 字符设备特殊文件
+#define I_NAMED_PIPE	0010000 // 命名管道文件
+#define I_SET_UID_BIT   0004000 // ”执行时设置有效用户“标志位
+#define I_SET_GID_BIT   0002000 // ”执行时设置有效组“标志位
 
 #endif
--- include/fcntl.h
+++ include/fcntl.h
@@ -1,51 +1,66 @@
 #ifndef _FCNTL_H
 #define _FCNTL_H
 
+/**
+ * 文件控制头文件：定义了fcntl函数，文件创建或打开中用到的一些选项
+ */
 #include <sys/types.h>
 
 /* open/fcntl - NOCTTY, NDELAY isn't implemented yet */
-#define O_ACCMODE	00003
-#define O_RDONLY	   00
-#define O_WRONLY	   01
-#define O_RDWR		   02
-#define O_CREAT		00100	/* not fcntl */
-#define O_EXCL		00200	/* not fcntl */
-#define O_NOCTTY	00400	/* not fcntl */
-#define O_TRUNC		01000	/* not fcntl */
-#define O_APPEND	02000
-#define O_NONBLOCK	04000	/* not fcntl */
+#define O_ACCMODE	00003 // 文件访问模式屏蔽码
+// open和fcntl使用的文件访问模式：同时只能使用其中之一
+#define O_RDONLY	   00 // 只读
+#define O_WRONLY	   01 // 只写
+#define O_RDWR		   02 // 读写
+// 下面是文件创建或操作标志，用于open，可于上面的访问模式用'|'方式一起使用
+#define O_CREAT		00100	/* not fcntl */ // 如果不存在，则创建
+#define O_EXCL		00200	/* not fcntl */ // 独占使用
+#define O_NOCTTY	00400	/* not fcntl */ // 不分配控制中断
+#define O_TRUNC		01000	/* not fcntl */ // 若文件已经存在，并且是写操作，则长度截断为0
+#define O_APPEND	02000 // 以添加方式打开，文件指针置为文件末尾
+#define O_NONBLOCK	04000	/* not fcntl */ // 以非阻塞方式打开和操作文件
 #define O_NDELAY	O_NONBLOCK
 
 /* Defines for fcntl-commands. Note that currently
  * locking isn't supported, and other things aren't really
  * tested.
  */
-#define F_DUPFD		0	/* dup */
-#define F_GETFD		1	/* get f_flags */
-#define F_SETFD		2	/* set f_flags */
-#define F_GETFL		3	/* more flags (cloexec) */
-#define F_SETFL		4
-#define F_GETLK		5	/* not implemented */
-#define F_SETLK		6
-#define F_SETLKW	7
+/*
+ * 文件描述符操作函数fcntl中的命令(cmd)
+ */
+#define F_DUPFD		0	/* dup */ // 拷贝文件句柄为最小数值的句柄
+#define F_GETFD		1	/* get f_flags */ // 获取文件描述符
+#define F_SETFD		2	/* set f_flags */ // 设置文件描述符
+#define F_GETFL		3	/* more flags (cloexec) */ // 取文件状态标志和访问模式
+#define F_SETFL		4 // 设置文件状态标志和访问模式
+#define F_GETLK		5	/* not implemented */ // 返回锁定文件的flock结构
+#define F_SETLK		6 // 设置[F_RDLCK或F_WRLCK]或清除(F_UNLCK)锁定
+#define F_SETLKW	7 // 等待设置或清除锁定
 
 /* for F_[GET|SET]FL */
+/*
+ * 执行 exec 时所需要关闭的文件描述符（句柄）： close on exec
+ * 用于F_GETFL和F_SETFL
+ * 
+ */
 #define FD_CLOEXEC	1	/* actually anything with low bit set goes */
 
 /* Ok, these are locking features, and aren't implemented at any
  * level. POSIX wants them.
  */
-#define F_RDLCK		0
-#define F_WRLCK		1
-#define F_UNLCK		2
+// 文件锁类型
+#define F_RDLCK		0 // 读锁
+#define F_WRLCK		1 // 写锁
+#define F_UNLCK		2 // 解锁
 
 /* Once again - not implemented, but ... */
+// 文件锁结构
 struct flock {
-	short l_type;
-	short l_whence;
-	off_t l_start;
-	off_t l_len;
-	pid_t l_pid;
+        short l_type; // 锁类型：F_RDLCK, F_WRLCK 或 F_UNLCK 
+        short l_whence; // 文件指针偏移：SEEK_SET, SEEK_CUR 或 SEEK_END
+        off_t l_start; // 阻塞锁定的开始处：相对偏移（字节数）
+        off_t l_len; // 阻塞锁定的长度：字节数，如果是0则到文件末尾
+        pid_t l_pid; // 加锁的进程id
 };
 
 extern int creat(const char * filename,mode_t mode);
