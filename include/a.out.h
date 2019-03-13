aa5a4f7fd7a9f0e62d39dee226433c4a7b06d8c5
klose911 <klose911@gmail.com>
2019-03-13 22:33:49 +0800

    a.out.h exec struct

--- include/a.out.h
+++ include/a.out.h
@@ -3,75 +3,96 @@
 
 #define __GNU_EXEC_MACROS__
 
+// 第一部分：关于目标文件执行(exec)结构以及相关宏
 struct exec {
-  unsigned long a_magic;	/* Use macros N_MAGIC, etc for access */
-  unsigned a_text;		/* length of text, in bytes */
-  unsigned a_data;		/* length of data, in bytes */
-  unsigned a_bss;		/* length of uninitialized data area for file, in bytes */
-  unsigned a_syms;		/* length of symbol table data in file, in bytes */
-  unsigned a_entry;		/* start address */
-  unsigned a_trsize;		/* length of relocation info for text, in bytes */
-  unsigned a_drsize;		/* length of relocation info for data, in bytes */
+        unsigned long a_magic; // 执行文件魔数
+        unsigned a_text; // 代码段长度，字节数
+        unsigned a_data; // 数据段长度，字节数
+        unsigned a_bss; // bss段长度，字节数
+        unsigned a_syms; // 符号表长度，字节数
+        unsigned a_entry; // 开始执行地址
+        unsigned a_trsize; // 代码重定位信息长度，字节数
+        unsigned a_drsize; // 数据重定位信息长度，字节数
 };
 
+// 取exec结构中的魔数域
 #ifndef N_MAGIC
 #define N_MAGIC(exec) ((exec).a_magic)
 #endif
 
+
 #ifndef OMAGIC
-/* Code indicating object file or impure executable.  */
+// 目标文件为老旧的可执行文件（不纯的）
 #define OMAGIC 0407
-/* Code indicating pure executable.  */
+// 目标文件为纯可执行文件：代码段在一个只读页，数据段在另外的读写页上
 #define NMAGIC 0410
-/* Code indicating demand-paged executable.  */
+// 目标文件为需要分页的可执行文件，其头结构占用开始处1K空间
 #define ZMAGIC 0413
 #endif /* not OMAGIC */
 
 #ifndef N_BADMAG
-#define N_BADMAG(x)					\
- (N_MAGIC(x) != OMAGIC && N_MAGIC(x) != NMAGIC		\
-  && N_MAGIC(x) != ZMAGIC)
+// 判断魔数是否可被识别，如果不是上面定义的这三种，返回“真”
+#define N_BADMAG(x)                                     \
+        (N_MAGIC(x) != OMAGIC && N_MAGIC(x) != NMAGIC   \
+         && N_MAGIC(x) != ZMAGIC)
 #endif
 
-#define _N_BADMAG(x)					\
- (N_MAGIC(x) != OMAGIC && N_MAGIC(x) != NMAGIC		\
-  && N_MAGIC(x) != ZMAGIC)
+#define _N_BADMAG(x)                                    \
+        (N_MAGIC(x) != OMAGIC && N_MAGIC(x) != NMAGIC   \
+         && N_MAGIC(x) != ZMAGIC)
 
+// exec结构末端到1024字节之间的长度
 #define _N_HDROFF(x) (SEGMENT_SIZE - sizeof (struct exec))
 
 #ifndef N_TXTOFF
-#define N_TXTOFF(x) \
- (N_MAGIC(x) == ZMAGIC ? _N_HDROFF((x)) + sizeof (struct exec) : sizeof (struct exec))
+// 计算“代码”部分起始偏移值
+// 如果是 ZMAGIC 那么代码部分从执行文件的1024字节偏移处开始( _N_HDROFF + sizeof(struct exec) )
+// 反之，直接从exec结构结尾处开始(32字节)
+#define N_TXTOFF(x)                                                     \
+        (N_MAGIC(x) == ZMAGIC ? _N_HDROFF((x)) + sizeof (struct exec) : sizeof (struct exec))
 #endif
 
 #ifndef N_DATOFF
+// 计算“数据”部分的起始偏移值：从“代码”部分末端开始
 #define N_DATOFF(x) (N_TXTOFF(x) + (x).a_text)
 #endif
 
 #ifndef N_TRELOFF
+// 计算“代码重定位信息”部分的起始偏移值：从“数据”部分末端开始
 #define N_TRELOFF(x) (N_DATOFF(x) + (x).a_data)
 #endif
 
 #ifndef N_DRELOFF
+// 计算“数据重定位信息”部分的起始偏移值：从“代码重定位信息”部分末端开始
 #define N_DRELOFF(x) (N_TRELOFF(x) + (x).a_trsize)
 #endif
 
 #ifndef N_SYMOFF
+// 计算“符号表”部分的起始偏移值：从“数据重定位信息”部分末端开始
 #define N_SYMOFF(x) (N_DRELOFF(x) + (x).a_drsize)
 #endif
 
 #ifndef N_STROFF
+// 计算“字符串”部分的起始偏移值：从“符号表”部分末端开始
 #define N_STROFF(x) (N_SYMOFF(x) + (x).a_syms)
 #endif
 
 /* Address of text segment in memory after it is loaded.  */
+// 下面对可执行文件被加载到内存（逻辑地址空间）的位置情况进行操作
 #ifndef N_TXTADDR
+// 代码段加载后在内存中的地址，代码段从地址0开始执行
 #define N_TXTADDR(x) 0
 #endif
 
 /* Address of data segment in memory after it is loaded.
    Note that it is up to you to define SEGMENT_SIZE
    on machines not listed here.  */
+/*
+ * 定义不同cpu架构下的段，页大小常量
+ *
+ * 注意：对于下面没有列出的cpu架构类型，需要你自己定义
+ * 
+ */
 #if defined(vax) || defined(hp300) || defined(pyr)
 #define SEGMENT_SIZE PAGE_SIZE
 #endif
@@ -89,35 +110,42 @@ struct exec {
 #define SEGMENT_SIZE PAGE_SIZE
 #endif
 
+// 对于386机器来说，页内存为4K，段大小为1K 
 #define PAGE_SIZE 4096
 #define SEGMENT_SIZE 1024
 
+// 以段为大小的“进位”方式
 #define _N_SEGMENT_ROUND(x) (((x) + SEGMENT_SIZE - 1) & ~(SEGMENT_SIZE - 1))
 
+// 内存中代码段的尾地址
 #define _N_TXTENDADDR(x) (N_TXTADDR(x)+(x).a_text)
 
 #ifndef N_DATADDR
-#define N_DATADDR(x) \
-    (N_MAGIC(x)==OMAGIC? (_N_TXTENDADDR(x)) \
-     : (_N_SEGMENT_ROUND (_N_TXTENDADDR(x))))
+// 计算内存中数据段的开始地址
+// 1. 如果是 OMAGIC，直接紧接在代码段之后
+// 2. 反之，从代码段后面的段边界开始（1KB边界对齐）        
+#define N_DATADDR(x)                                \
+        (N_MAGIC(x)==OMAGIC? (_N_TXTENDADDR(x))     \
+         : (_N_SEGMENT_ROUND (_N_TXTENDADDR(x))))
 #endif
 
 /* Address of bss segment in memory after it is loaded.  */
 #ifndef N_BSSADDR
+// 计算内存中bss段开始地址
 #define N_BSSADDR(x) (N_DATADDR(x) + (x).a_data)
 #endif
 
 #ifndef N_NLIST_DECLARED
 struct nlist {
-  union {
-    char *n_name;
-    struct nlist *n_next;
-    long n_strx;
-  } n_un;
-  unsigned char n_type;
-  char n_other;
-  short n_desc;
-  unsigned long n_value;
+        union {
+                char *n_name;
+                struct nlist *n_next;
+                long n_strx;
+        } n_un;
+        unsigned char n_type;
+        char n_other;
+        short n_desc;
+        unsigned long n_value;
 };
 #endif
 
@@ -192,27 +220,27 @@ struct nlist {
 
 struct relocation_info
 {
-  /* Address (within segment) to be relocated.  */
-  int r_address;
-  /* The meaning of r_symbolnum depends on r_extern.  */
-  unsigned int r_symbolnum:24;
-  /* Nonzero means value is a pc-relative offset
-     and it should be relocated for changes in its own address
-     as well as for changes in the symbol or section specified.  */
-  unsigned int r_pcrel:1;
-  /* Length (as exponent of 2) of the field to be relocated.
-     Thus, a value of 2 indicates 1<<2 bytes.  */
-  unsigned int r_length:2;
-  /* 1 => relocate with value of symbol.
-          r_symbolnum is the index of the symbol
-	  in file's the symbol table.
-     0 => relocate with the address of a segment.
-          r_symbolnum is N_TEXT, N_DATA, N_BSS or N_ABS
-	  (the N_EXT bit may be set also, but signifies nothing).  */
-  unsigned int r_extern:1;
-  /* Four bits that aren't used, but when writing an object file
-     it is desirable to clear them.  */
-  unsigned int r_pad:4;
+        /* Address (within segment) to be relocated.  */
+        int r_address;
+        /* The meaning of r_symbolnum depends on r_extern.  */
+        unsigned int r_symbolnum:24;
+        /* Nonzero means value is a pc-relative offset
+           and it should be relocated for changes in its own address
+           as well as for changes in the symbol or section specified.  */
+        unsigned int r_pcrel:1;
+        /* Length (as exponent of 2) of the field to be relocated.
+           Thus, a value of 2 indicates 1<<2 bytes.  */
+        unsigned int r_length:2;
+        /* 1 => relocate with value of symbol.
+           r_symbolnum is the index of the symbol
+           in file's the symbol table.
+           0 => relocate with the address of a segment.
+           r_symbolnum is N_TEXT, N_DATA, N_BSS or N_ABS
+           (the N_EXT bit may be set also, but signifies nothing).  */
+        unsigned int r_extern:1;
+        /* Four bits that aren't used, but when writing an object file
+           it is desirable to clear them.  */
+        unsigned int r_pad:4;
 };
 #endif /* no N_RELOCATION_INFO_DECLARED.  */
 
